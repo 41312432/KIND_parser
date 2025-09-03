52e601d3cf959f07ca1bf92f18c2beb69e432954
Wansik Jo <wnsk.jo@gmail.com>
2025-09-03 01:15:51 +0000

    feat: Add DocumentProvider

---
+++ utils/constants.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+class FileType:
+    MAIN_SHEET = "mainSheet"
+    SPECIAL_SHEET = "specialSheet"
+
+class FolderName:
+    ATTACHMENT = "별표"
+    LAW = "법률"
+
+class FileName:
+    TOC = "gwan_toc.txt"
+    CONTENT = "content.md"
+    TERMS_FILE_LIST = "termsFileList.txt" 
