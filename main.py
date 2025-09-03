4fcf6b84a9058ee3918b8690df0df2d6a1e83bb5
Wansik Jo <wnsk.jo@gmail.com>
2025-09-03 02:58:19 +0000

    fix: Fix import error

--- main.py
+++ main.py
@@ -2,24 +2,27 @@ import sys
 import os
 import logging
 from pathlib import Path
+import ops_logging
+
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from utils.arg_parser import get_args
 from core.orchestrator import PipelineOrchestrator
 
-from services.pdf_converter import PDFConverter
-from services.document_provider import DocumentProvider
-from services.vlm_table_processor import VLMTableProcessor
-from services.content_structurer import ContentStructurer
+from service_object.pdf_converter import PDFConverter
+from service_object.document_provider import DocumentProvider
+from service_object.vlm_table_processor import VLMTableProcessor
+from service_object.content_structurer import ContentStructurer
 
 from process.pdf_parsing import PDFParsing
 from process.table_processing import TableProcessing
 from process.content_structuring import ContentStructuring
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s',
-                    handlers=[logging.StreamHandler(sys.stdout)], force=True)
-logger = logging.getLogger(__name__)
+# ops_logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s',
+#                     handlers=[logging.StreamHandler(sys.stdout)], force=True)
+
+logger = ops_logging.get_logger("service")
 
 def main():
     logger.info("Starting processing pipeline...")
@@ -51,7 +54,7 @@ def main():
         logger=logger
     )
 
-    orchestrator = PipelineOrchestrator(process=[pdf_conversion, vlm_table_processing, content_structuring,])
+    orchestrator = PipelineOrchestrator(process=[pdf_conversion, vlm_table_processing, content_structuring])
     
     initial_context = {
         "output_dir": Path(args.output_dir),
